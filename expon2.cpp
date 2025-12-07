22e2d08455171b83cf545a5b223f6a91818c88d4
IshitaRoy20 <ishitaroy20042005@gmail.com>
2025-12-07 23:27:07 +0530

    31

---
+++ expon2.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ll long long
+ll mod = 1e9 + 7;
+using namespace std;
+ll power(ll a, ll b, ll c)
+{
+    if (b == 0)
+        return 1;
+    if (b == 1)
+        return a % c;
+    ll tmp = power(a, b / 2, c);
+    if (b % 2 == 0)
+        return (tmp * tmp) % c;
+    else
+        return (((tmp * tmp) % c) * a) % c;
+}
+int main()
+{
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        long long a, b, c;
+        cin >> a >> b >> c;
+        long long x = power(b, c, mod - 1);
+        cout << power(a, x, mod) << endl;
+    }
+}
