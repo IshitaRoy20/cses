b4bc69902910f9f042dbd13f354ee5710e57c180
IshitaRoy20 <ishitaroy20042005@gmail.com>
2025-12-08 22:15:58 +0530

    36

---
+++ playlist.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+        cin >> a[i];
+    ll l = 0, r = 0;
+    map<ll, ll> ind;
+    ll mx = 0;
+    while (r < a.size())
+    {
+        l = max(ind[a[r]], l);
+        mx = max(mx, r - l + 1);
+        ind[a[r]] = r + 1;
+        r++;
+    }
+    cout << mx;
+}
