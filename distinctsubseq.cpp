56ac7737f41515c8994233dcbd8b882ab8ba139c
IshitaRoy20 <ishitaroy20042005@gmail.com>
2025-12-14 22:21:33 +0530

    14dec_13

--- distinctsubseq.cpp
+++ distinctsubseq.cpp
@@ -1,23 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-int mod=1e9+7;
+int mod = 1e9 + 7;
+
 int main()
 {
     ll n;
     cin >> n;
+
     vector<ll> a(n);
-    unordered_map<ll,ll>mp;
+    unordered_map<ll, ll> mp;
+
     for (ll i = 0; i < n; i++)
     {
         cin >> a[i];
         mp[a[i]]++;
     }
-    ll ans=1;
-    for(int i=0;i<n;i++)
+
+    ll ans = 1;
+    for (auto &p : mp)
     {
-        ans=(ans%mod*(mp[i]+1)%mod)%mod;
+        ans = (ans * (p.second + 1)) % mod;
     }
-    ans=(ans-1+mod)%mod;
-    cout<<ans;
+
+    ans = (ans - 1 + mod) % mod;
+    cout << ans;
 }
