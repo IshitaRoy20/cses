d087d68c35b5cf4f0693e0bce3c6c107cd465c25
IshitaRoy20 <ishitaroy20042005@gmail.com>
2025-12-28 22:21:00 +0530

    84

---
+++ knightmoves.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<vector<int>> grid(n, vector<int>(n, INT_MAX));
+    int dx[8] = {-2, -2, -1, -1, 1, 1, 2, 2};
+    int dy[8] = {-1, 1, -2, 2, -2, 2, -1, 1};
+
+    queue<pair<int, int>> q;
+    grid[0][0] = 0;
+    q.push({0, 0});
+
+    while (!q.empty())
+    {
+        auto [x, y] = q.front();
+        q.pop();
+
+        for (int i = 0; i < 8; i++)
+        {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n)
+            {
+                if (grid[nx][ny] > grid[x][y] + 1)
+                {
+                    grid[nx][ny] = grid[x][y] + 1;
+                    q.push({nx, ny});
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << grid[i][j] << " ";
+        cout << endl;
+    }
+}
